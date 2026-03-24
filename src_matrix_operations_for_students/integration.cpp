fb487af725c83cd553ddcd99cd272a4d252a465f
Felix Lindström <felixwlindstrom@gmail.com>
2026-03-24 08:48:38 +0100

    implementerade integration med reduce

--- src_matrix_operations_for_students/integration.cpp
+++ src_matrix_operations_for_students/integration.cpp
@@ -42,13 +42,26 @@ double parallel_num_integral(int n) {
     return result;
 }
 
+double reduce_num_integral(int n) {
+    double res = 0;
+    #pragma omp parallel for reduction(+:res)
+    for (int i = 0; i < n; i++) {
+        double xi = (i + 0.5)/n;
+        res += f(xi)/n;
+    }
+    return res;
+}
 
 
-int find_n_accurate() {
+
+int find_n_accurate(int method) {
     double correct = 1.0;
     int n = 1;
     while (true) {
-        double res = parallel_num_integral(n);
+        double res;
+        if (method == 0) res = simple_num_integral(n);
+        else if (method == 1) res = parallel_num_integral(n);
+        else res = reduce_num_integral(n);
         if (abs(correct - res) < 0.00000001)
             break;
         n++;
@@ -58,11 +71,36 @@ int find_n_accurate() {
 
 int main() {
     omp_set_num_threads(NUM_THREADS);
-    Stopwatch stopwatch;
-    stopwatch.start();
-    int n = find_n_accurate();
-    stopwatch.stop();
-    double elapsed_time = stopwatch();
-    std::cout << "n = " << n << std::endl;
-    std::cout << "Time elapsed: " << elapsed_time << "s" << std::endl;
+    Stopwatch stopwatch_simple;
+    Stopwatch stopwatch_parallel;
+    Stopwatch stopwatch_reduce;
+
+
+    // Benchmark simple integration
+    stopwatch_simple.start();
+    int n_simple = find_n_accurate(0);
+    stopwatch_simple.stop();
+
+    // Benchmark hand-written reduce
+    stopwatch_parallel.start();
+    int n_parallel = find_n_accurate(1);
+    stopwatch_parallel.stop();
+
+    // Benchmark omp reduce
+    stopwatch_reduce.start();
+    int n_reduce = find_n_accurate(2);
+    stopwatch_reduce.stop();
+
+
+    std::cout << "Results for simple integration:\n";
+    std::cout << "n = " << n_simple << "\n";
+    std::cout << "Time elapsed: " << stopwatch_simple() << "s\n\n";
+
+    std::cout << "Results for hand-written reduce:\n";
+    std::cout << "n = " << n_parallel << "\n";
+    std::cout << "Time elapsed: " << stopwatch_parallel() << "s\n\n";
+
+    std::cout << "Results for omp reduce:\n";
+    std::cout << "n = " << n_reduce << "\n";
+    std::cout << "Time elapsed: " << stopwatch_reduce() << "s" << std::endl;
 }
