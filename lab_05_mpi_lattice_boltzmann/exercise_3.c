c19aec9facf25ac34d42bd9966e85e88527b1ac1
Lindström Felix <lindstrf@im2ag-216-13.u-ga.fr>
2026-04-29 15:07:06 +0200

    non blocking

--- lab_05_mpi_lattice_boltzmann/exercise_3.c
+++ lab_05_mpi_lattice_boltzmann/exercise_3.c
@@ -32,16 +32,35 @@ void lbm_comm_init_ex3(lbm_comm_t * comm, int total_width, int total_height)
 /****************************************************/
 void lbm_comm_ghost_exchange_ex3(lbm_comm_t * comm, lbm_mesh_t * mesh)
 {
-	//
-	// TODO: Implement the 1D communication with non-blocking MPI functions.
-	//
-	// To be used:
-	//    - DIRECTIONS: the number of doubles composing a cell
-	//    - double[9] lbm_mesh_get_cell(mesh, x, y): function to get the address of a particular cell.
-	//    - comm->width : The with of the local sub-domain (containing the ghost cells)
-	//    - comm->height : The height of the local sub-domain (containing the ghost cells)
-	
-	//example to access cell
-	//double * cell = lbm_mesh_get_cell(mesh, local_x, local_y);
-	//double * cell = lbm_mesh_get_cell(mesh, comm->width - 1, 0);
+	//check if not in leftmost pos
+	for(int i = 0; i < comm->height; i++){
+		MPI_Request requests[4] = {
+			MPI_REQUEST_NULL,
+			MPI_REQUEST_NULL,
+			MPI_REQUEST_NULL,
+			MPI_REQUEST_NULL
+		};
+
+		// send left
+		if(comm->rank_x !=0){
+			double * cell = lbm_mesh_get_cell(mesh, 1, i);
+			MPI_Isend((void*)cell, DIRECTIONS, MPI_DOUBLE, comm->rank_x-1,0,MPI_COMM_WORLD, &requests[0]);
+		} 
+		// receive right
+		if(comm->rank_x != comm->nb_x-1){
+			double * cell = lbm_mesh_get_cell(mesh, comm->width-1, i);
+			MPI_Irecv((void*)cell, DIRECTIONS, MPI_DOUBLE, comm->rank_x+1,0,MPI_COMM_WORLD, &requests[1]);
+		}
+		// send right
+		if(comm->rank_x != comm->nb_x-1){
+			double * cell = lbm_mesh_get_cell(mesh, comm->width-2, i);
+			MPI_Isend((void*)cell, DIRECTIONS, MPI_DOUBLE, comm->rank_x+1,1,MPI_COMM_WORLD, &requests[2]);
+		}
+		// receive left
+		if(comm->rank_x !=0){
+			double * cell = lbm_mesh_get_cell(mesh, 0, i);
+			MPI_Irecv((void*)cell, DIRECTIONS, MPI_DOUBLE, comm->rank_x-1,1,MPI_COMM_WORLD, &requests[3]);
+		}
+		MPI_Waitall(4, requests, MPI_STATUSES_IGNORE);
+	}
 }
